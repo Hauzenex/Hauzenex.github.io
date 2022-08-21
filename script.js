5adb950d695bf6b90ac8f63278993f4040cbf783
Hauzenex <106713533+Hauzenex@users.noreply.github.com>
2022-08-21 10:44:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function updateCounter() {
-    fetch('https://api.countapi.xyz/update/uimonk/youtubechannel/?amount=1')
+    fetch('https://api.countapi.xyz/update/hauzenex.github.io/youtubechannel/?amount=1')
         .then(res => res.json())
         .then(data => counterElement.innerHTML = data.value)
 }
